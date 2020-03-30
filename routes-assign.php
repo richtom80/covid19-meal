1ca8391ce8d23301d41a67af31763ed350ea7332
Richard Thompson <richardthompson@Richards-MacBook-Pro.local>
2020-03-30 21:00:06 +0100

    GMaps API: 2020-03-30 21:00

--- routes-assign.php
+++ routes-assign.php
@@ -7,21 +7,10 @@ $page_title = "Route Assign ".$r['name'];
 
 if(!empty($_POST)){
 
-  $ri = $db->prepare("INSERT INTO `routes`(`name`, `kitchen`, `mon`, `tue`, `wed`, `thu`, `fri`, `sat`, `sun`)
-  VALUES (:name,:kitchen,:mon,:tue,:wed,:thu,:fri,:sat,:sun);");
-
-  $ri->execute(array(
-    ":name" => $_POST['name'],
-    ":kitchen" => $_POST['kitchen'],
-    ":mon" => $_POST['mon'],
-    ":tue" => $_POST['tue'],
-    ":wed" => $_POST['wed'],
-    ":thu" => $_POST['thu'],
-    ":fri" => $_POST['fri'],
-    ":sat" => $_POST['sat'],
-    ":sun" => $_POST['sun']
-  ));
-
+  $us = $db->prepare("UPDATE `sites` SET `route` = ? WHERE `id` = ?;");
+  foreach($_POST['siteAssign'] as $site){
+    $us->execute(array($_GET['id'], $site));
+  }
 }
 
 require 'includes/html-head.php';
@@ -50,48 +39,49 @@ require 'includes/html-head.php';
               <div id="map" style="height: 700px;">Map Loading&hellip;</div>
             </div>
           </div>
+          <form method="post">
+            <div class="card shadow mb-4">
+              <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Assign Sites</h6>
+              </div>
+              <div class="card-body">
+                <div class="table-responsive">
+                  <table class="table table-bordered" width="100%" cellspacing="0">
+                    <thead>
+                      <tr>
+                        <th>Name</th>
+                        <th>Phone</th>
+                        <th>Type</th>
+                        <th>Postcode</th>
+                        <th># Std</th>
+                        <th># Veg</th>
+                        <th>DOW</th>
+                        <th>Route</th>
+                        <th>Assign</th>
+                      </tr>
+                    </thead>
 
-          <div class="card shadow mb-4">
-            <div class="card-header py-3">
-              <h6 class="m-0 font-weight-bold text-primary">Assign Sites</h6>
-            </div>
-            <div class="card-body">
-              <div class="table-responsive">
-                <table class="table table-bordered" width="100%" cellspacing="0">
-                  <thead>
-                    <tr>
-                      <th>Name</th>
-                      <th>Phone</th>
-                      <th>Type</th>
-                      <th>Postcode</th>
-                      <th># Std</th>
-                      <th># Veg</th>
-                      <th>DOW</th>
-                      <th>Route</th>
-                      <th>Assign</th>
-                    </tr>
-                  </thead>
-
-                  <tbody>
-                    <?php foreach($ms->listSites() as $site){ ?>
-                    <tr>
-                      <td><?= $site['name']." ".$site['surname']; ?></td>
-                      <td><?= $site['phone']; ?></td>
-                      <td><?= ucwords($site['type']); ?></td>
-                      <td><?= $site['postcode']; ?></td>
-                      <td><?= $site['std_meal']; ?></td>
-                      <td><?= $site['veg_meal']; ?></td>
-                      <td><?= $ms->dowOut($site); ?></td>
-                      <td><?= $ms->routeOut($site['route'], $_GET['id']); ?></td>
-                      <td><input type="checkbox" name="site_<?= $site['id']; ?>" <?php if($site['route'] == $_GET['id']) echo "checked"; ?>/></td>
-                    </tr>
-                    <?php } ?>
-                  </tbody>
-                </table>
+                    <tbody>
+                      <?php foreach($ms->listSites() as $site){ ?>
+                      <tr>
+                        <td><?= $site['name']." ".$site['surname']; ?></td>
+                        <td><?= $site['phone']; ?></td>
+                        <td><?= ucwords($site['type']); ?></td>
+                        <td><?= $site['postcode']; ?></td>
+                        <td><?= $site['std_meal']; ?></td>
+                        <td><?= $site['veg_meal']; ?></td>
+                        <td><?= $ms->dowOut($site); ?></td>
+                        <td><?= $ms->routeOut($site['route'], $_GET['id']); ?></td>
+                        <td><input type="checkbox" name="siteAssign[]" id="site_<?= $site['id']; ?>" value="<?= $site['id']; ?>" <?php if($site['route'] == $_GET['id']) echo "checked disabled"; ?>/></td>
+                      </tr>
+                      <?php } ?>
+                    </tbody>
+                  </table>
+                  <button type="submit" class="btn btn-success">Assign Sites</button>
+                </div>
               </div>
             </div>
-          </div>
-
+          </form>
         </div>
         <!-- /.container-fluid -->
 
@@ -137,24 +127,37 @@ require 'includes/html-head.php';
       <?php
       foreach($ms->listSites() as $s){
         $colour = 'ffdddd';
-        if(!empty($s['hexcode'])) $colour = $s['hexcode'];
-        echo "addMarker('{$s['id']}', '{$s['lat']}','{$s['lng']}', '{$colour}', '".str_replace("'", "", $s['name']." ".$s['surname'])." [{$s['postcode']}]');\n";
-      }
-       ?>
-       var markerCluster = new MarkerClusterer(map, [],
-         {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
-      markerCluster.addMarkers(markers, true);
+        if($s['route'] == $_GET['id']){ $colour = '44ff55'; }
+        echo "addMarker('{$s['id']}', '{$s['lat']}','{$s['lng']}', '$colour', '".str_replace("'", "", $s['name']." ".$s['surname'])." [{$s['postcode']}]');\n";
+      } ?>
+      var markerCluster = new MarkerClusterer(map, [],{
+       imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m',
+       maxZoom: 12
+      });
+
     }
+    var i = 0;
     function addMarker(id, glat, glng, colour, info = "Unlabelled"){
       var marker = new google.maps.Marker({
+        id: id,
+        index: i,
         position: new google.maps.LatLng(glat,glng),
         title: info,
         icon: '//chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|'+colour,
         map: map
       });
+      i++;
 
+      marker.addListener('click', function(m) {
+        $('#site_'+id).prop('checked', true);
+        clearMarker(this.index);
+      });
       markers.push(marker);
     }
+    function clearMarker(id){
+      markers[id].setMap(null);
+    }
+
   </script>
   <script src="https://maps.googleapis.com/maps/api/js?key=<?= GOOGLE_API_JS; ?>&callback=initMap" async defer></script>
 
