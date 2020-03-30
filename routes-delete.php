b2857bcaf389097d4ecd4765f634f0f7033a34ca
Richard Thompson <richardthompson@Richards-MacBook-Pro.local>
2020-03-30 22:58:07 +0100

    Route Update: 2020-03-30 22:57

--- routes-delete.php
+++ routes-delete.php
@@ -2,6 +2,7 @@
 require 'includes/preload.php';
 
 if(!empty($_GET['id'])){
-  $db->query("DELETE FROM `routes` WHERE `id` = {$_GET['id']};");
+  $db->query("DELETE FROM `routes` WHERE `id` = {$_GET['id']};
+  UPDATE `sites` SET `route` = NULL WHERE `route` = {$_GET['id']}");
 }
 header("Location: routes.php");
